9efa04b4a767854e1847928077dbc735eeaad29a
Steffen Gebert <steffen.gebert@emnify.com>
2022-09-30 15:52:53 +0200

    Output for ENI of client instance

--- modules/topology/outputs.tf
+++ modules/topology/outputs.tf
@@ -3,6 +3,11 @@ output "left_instance_id" {
   value       = module.instance_left.id
 }
 
+output "left_instance_eni" {
+  description = "ENI ID in of the client instance"
+  value       = module.instance_left.primary_network_interface_id
+}
+
 output "right_apigw_vpce_eni" {
   description = "ENI ID of the VPC endpoint of the private API Gateway in the right VPC"
   value       = one(aws_vpc_endpoint.api_gateway.network_interface_ids)
