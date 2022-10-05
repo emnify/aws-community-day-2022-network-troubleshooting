9661226b549500e3c3fbd14dd795395a82bfed98
Steffen Gebert <steffen.gebert@emnify.com>
2022-10-05 23:50:37 +0200

    output TGW ID

--- modules/topology/outputs.tf
+++ modules/topology/outputs.tf
@@ -12,3 +12,7 @@ output "right_apigw_vpce_eni" {
   description = "ENI ID of the VPC endpoint of the private API Gateway in the right VPC"
   value       = one(aws_vpc_endpoint.api_gateway.network_interface_ids)
 }
+
+output "transit_gateway_id" {
+  value = module.tgw_middle_right.ec2_transit_gateway_id
+}
