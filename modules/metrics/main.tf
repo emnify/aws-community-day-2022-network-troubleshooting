db52baadee57e830402ba1ad13753166180657a9
Steffen Gebert <steffen.gebert@emnify.com>
2022-10-12 00:12:42 +0200

    fix cloudwatch b/c merging is hard

--- modules/metrics/main.tf
+++ modules/metrics/main.tf
@@ -2,6 +2,7 @@ resource "aws_cloudwatch_dashboard" "this" {
   dashboard_name = "Trouble-TGW"
   dashboard_body = templatefile(
     "${path.module}/dashboard.tpl", {
+      region           = data.aws_region.current.name
       tgw_id           = var.tgw_id
       tgw_attach_left  = var.tgw_attachments[0]
       tgw_attach_right = var.tgw_attachments[1]
